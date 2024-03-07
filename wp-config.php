25c3015e07475f4f3e62bea374e6dd8001ba1b1b
Harry Finn <harry@harryfinn.co.uk>
2024-03-07 13:07:11 +0000

    Initial composer rework for v3.x.x

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,16 @@
 <?php
 
-use Symfony\Component\Debug\Debug;
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+
+$table_prefix = 'wp_';
+
+/* Add any custom values between this line and the "stop editing" line. */
+
 use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -8,30 +18,23 @@ require __DIR__ . '/vendor/autoload.php';
 $env = new Dotenv();
 $env->load(__DIR__ . '/.env');
 
-/**
- * If we are in DEBUG mode, enable symfony debug component
- */
-if (filter_var(getenv('WP_DEBUG'), FILTER_VALIDATE_BOOLEAN) === true) {
-    Debug::enable();
-}
-
 foreach (explode(',', $_ENV['SYMFONY_DOTENV_VARS']) as $env_var) {
-    define($env_var, filter_var($_ENV[$env_var], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ?? $_ENV[$env_var]);
+    define(
+        $env_var,
+        filter_var(
+            $_ENV[$env_var],
+            FILTER_VALIDATE_BOOLEAN,
+            FILTER_NULL_ON_FAILURE
+        ) ?? $_ENV[$env_var]
+    );
 }
 
-define('AS3CF_SETTINGS', serialize(array(
-    'provider' => 'aws',
-    'access-key-id' => getenv('S3_AWS_ID'),
-    'secret-access-key' => getenv('S3_AWS_SECRET'),
-)));
-
-$table_prefix = 'wp_';
-
-/* That's all, stop editing! Happy blogging. */
+/* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if (!defined('ABSPATH'))
-    define('ABSPATH', dirname(__FILE__) . '/');
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
+}
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once ABSPATH . 'wp-settings.php';
